91c738251724fdd8ecc8ddc4cd841c0864c83b6a
VinhHuynh <huynhquangvinh01121999@gmail.com>
2021-09-28 17:24:23 +0700

    update socket deploy

--- HandleSocket/Main.js
+++ HandleSocket/Main.js
@@ -6,16 +6,16 @@ module.exports = (io) => {
   io.on("connection", (client) => {
     // console.log(`${client.id} connected`);
 
-    client.on("updateUserInbox", (data) => {
-      // console.log(data);
-      io.emit("reUpdateUserInbox", { data, ClientId: client.id });
-    });
-
+    // Bước 1
     // khi client connnect vào thì gửi idClient qua cho CLient
     client.emit("sendClientId", client.id);
 
+    // Bước 4
     // lắng nghe khi client phản hồi sau khi server gửi id qa -> tiến hành update lại clientId cho user connect
     client.on("replySendClientId", (userInfo) => {
+      // console.log("Bước 2 - " + userInfo + "-" + client.id);
+
+      // Bước 5: phản hồi để client cập nhật lại ds các user online
       io.emit("updateReducerUsers_ClientId", {
         clientId: client.id,
         userInfo: userInfo,
@@ -23,17 +23,17 @@ module.exports = (io) => {
       });
 
       // push userInfo vô LIST_CLIENT để khỏi mock db
-      if (LIST_CLIENT.length == 0) {
-        LIST_CLIENT.push({ userInfo: userInfo, clientId: client.id });
-      } else {
-        LIST_CLIENT.forEach((item) => {
-          if (item.userInfo != userInfo) {
-            LIST_CLIENT.push({ userInfo: userInfo, clientId: client.id });
-          } else {
-            item.clientId = client.id;
-          }
-        });
-      }
+      // if (LIST_CLIENT.length == 0) {
+      //   LIST_CLIENT.push({ userInfo: userInfo, clientId: client.id });
+      // } else {
+      //   LIST_CLIENT.forEach((item) => {
+      //     if (item.userInfo != userInfo) {
+      //       LIST_CLIENT.push({ userInfo: userInfo, clientId: client.id });
+      //     } else {
+      //       item.clientId = client.id;
+      //     }
+      //   });
+      // }
 
       // cập nhật lại trong db
       Users.findOneAndUpdate(
@@ -55,24 +55,18 @@ module.exports = (io) => {
         });
     });
 
-    // gửi thông báo cho các Client khác là có một user mới disconnect
+    // Bước 7: nghe client disconnect (tắt trình duyệt)
     client.on("disconnecting", (data) => {
       // LIST_CLIENT.splice(LIST_CLIENT.indexOf(client), 1);
       // console.log(LIST_CLIENT);
+
+      // Bước 8: gửi thông báo cho các Client khác là có một user mới disconnect (tắt trình duyệt)
       io.emit("notifiDisconnect", client.id);
     });
 
-    // lắng nghe khi client Login vô -> update lại trạng thái cho user đó online
-    client.on("setOnlineLogin", (data) => {
-      io.emit("replySetOnline", {
-        clientId: client.id,
-        userInfo: data,
-        status: true,
-      });
-    });
-
-    // client phản hồi user này đã disconnect và tiến hành update lại status trong db
+    // Bước 11: nhận phản hồi từ client xác nhận hủy kết nối - tiến hành update lại status trong db
     client.on("acceptDisconnect", (clientId) => {
+      // Bước 12: Gửi thông báo đến các client khác để update lại state status
       io.emit("clientLogouted", clientId);
       Users.findOneAndUpdate(
         {
@@ -88,6 +82,23 @@ module.exports = (io) => {
           // console.log(data);
         })
         .catch((err) => {});
+    });
+
+    // Từ bước này trở đi là dành cho các thao tác
+
+    // Bước 12
+    // lắng nghe khi client Login vô -> update lại trạng thái cho user đó online
+    client.on("setOnlineLogin", (data) => {
+      io.emit("replySetOnline", {
+        clientId: client.id,
+        userInfo: data,
+        status: true,
+      });
+    });
+
+    client.on("updateUserInbox", (data) => {
+      // console.log(data);
+      io.emit("reUpdateUserInbox", { data, ClientId: client.id });
     });
 
     client.on("saveMessToDb", (data) => {
@@ -107,12 +118,15 @@ module.exports = (io) => {
         });
     });
 
-    client.on("sendMess", (data) => {
+    client.on("send-mess", (data) => {
+      const clientId = data.clientId;
       // var result = LIST_CLIENT.filter(
       //   (client) => client.userInfo === data.ToUser
       // );
-      console.log(data.userInbox.ClientId);
-      io.to(data.userInbox.ClientId).emit("replySendMess", data);
+      // console.log(data.userInbox.ClientId);
+      io.to(clientId).emit("recei-mess", data.request);
+      // client.emit("recei-mess", { ClientId: client.id, Content: data });
+      // io.emit("recei-mess", data.request);
     });
   });
 };
